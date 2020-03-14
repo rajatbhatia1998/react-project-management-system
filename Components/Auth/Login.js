334cfa562890895928eb3697a33731ea88923881
rajatbhatia1998 <rajat.bhatia859@gmail.com>
2020-03-14 15:28:30 +0530

    2 files modified

--- Components/Auth/Login.js
+++ Components/Auth/Login.js
@@ -42,9 +42,9 @@ ui.start('#firebaseui-auth-container', uiConfig);
   return(
     <div>
 
-    <h1>Welcome to My Awesome App</h1>
-    <div id="firebaseui-auth-container"></div>
-    <div id="loader">Loading...</div>
+    <h1>Welcome to LOGIN PAGE</h1>
+        <div id="firebaseui-auth-container"></div>
+        <div id="loader">Loading...</div>
 
     </div>
   )
--- index.js
+++ index.js
@@ -37,6 +37,10 @@ class App extends Component {
                   <Login/>
             </Route>
 
+             <Route path="/" >
+                  <Login/>
+            </Route>
+
 
          </Router>
       </div>
